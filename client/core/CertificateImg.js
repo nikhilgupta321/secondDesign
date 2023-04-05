7cf3800879fbe1b4b18acae1ec42e4a670f31ba6
nikhilgupta321 <nikhilgupta321@gmail.com>
2023-04-05 11:35:02 +0530

    checking auto merge and deployment

--- client/core/CertificateImg.js
+++ client/core/CertificateImg.js
@@ -4,7 +4,7 @@ import Card from "./Card";
 export default function Certificate(props) {
   return (
     <Card cardTitle="CERTIFICATE">
-        <img className="sidebar-img" src="/assets/images/certificate.jpg" />
-      </Card>
+      <img className="sidebar-img" src="/assets/images/certificate.jpg" />
+    </Card>
   );
 }
