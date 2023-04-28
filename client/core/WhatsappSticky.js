627d96ad0017b0e0bac118db85a1a52aa3544fa7
nikhilgupta321 <nikhilgupta321@gmail.com>
2023-04-28 10:30:56 +0530

    whatsapp message

--- client/core/WhatsappSticky.js
+++ client/core/WhatsappSticky.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalContext";
 
 export default function WhatsappSticky(props) {
+  const { settings } = useContext(GlobalContext)
   return (
-    <a href="https://wa.me/919999888671?text=Hi, I have a query regarding publication in International Journal of Multidisciplinary Research and Development.">
+    <a href={`https://wa.me/919999888671?text=Hi, I have a query regarding publication in ${settings.websitename} (${settings.domain})`}>
       <img src="/assets/images/whatsapp-logo.png" className="blinkBox fixed right-0 top-96 -rotate-90 origin-bottom-right w-40 h-auto" />
     </a>
   );
