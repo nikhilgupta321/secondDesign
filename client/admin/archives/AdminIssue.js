b5ca39b7fc211c5d660921fd30462eb789a61c31
nikhilgupta321 <nikhilgupta321@gmail.com>
2023-07-21 04:14:37 +0000

    title heading

--- client/admin/archives/AdminIssue.js
+++ client/admin/archives/AdminIssue.js
@@ -1,130 +1,207 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
-import auth from "../../helper/auth-helper"
+import auth from "../../helper/auth-helper";
 import { listAdminIssue } from "../../helper/api-archives";
 import { decode } from "html-entities";
 import { getIndexPage } from "../../helper/api-pdf";
 import { getbulkCertificates } from "../../helper/api-pdf";
+import sanitizeHtml from "sanitize-html";
+
+const cleanHtml = (data) => {
+  let cleanData = sanitizeHtml(data, {
+    allowedTags: ["i", "em"],
+    allowedAttributes: {},
+  });
+  return cleanData.replace(/\s+/g, " ");
+};
 
 const parseDate = (date) => {
-  const d = new Date(date)
-  return d.toLocaleString()
-}
+  const d = new Date(date);
+  return d.toLocaleString();
+};
 
 const saveIndexPage = (selected) => {
   getIndexPage(selected).then((blob) => {
     const url = window.URL.createObjectURL(blob);
-    const link = document.createElement('a');
+    const link = document.createElement("a");
     link.href = url;
     link.download = `Index-Page.pdf`;
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
     window.URL.revokeObjectURL(url);
-  })
-}
+  });
+};
 
 const saveBulkCertificates = (selected) => {
   getbulkCertificates(selected).then((blob) => {
     const url = window.URL.createObjectURL(blob);
-    const link = document.createElement('a');
+    const link = document.createElement("a");
     link.href = url;
     link.download = `Certificates.zip`;
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
     window.URL.revokeObjectURL(url);
-  })
-}
-
+  });
+};
 
 export default function AdminIssue(props) {
   const { year, vol, issue } = useParams();
-  const [issues , setIssues] = useState([])
-  const [selected, setSelected] = useState([])
-  
-  const jwt = auth.isAuthenticated()
+  const [issues, setIssues] = useState([]);
+  const [selected, setSelected] = useState([]);
+
+  const jwt = auth.isAuthenticated();
 
   const handleSelect = (refnumber) => {
-    if(selected.includes(refnumber)) {
-      setSelected(selected.filter((item) => item !== refnumber))
+    if (selected.includes(refnumber)) {
+      setSelected(selected.filter((item) => item !== refnumber));
+    } else {
+      setSelected([...selected, refnumber]);
     }
-    else {
-      setSelected([...selected, refnumber])
-    }
-  }
+  };
 
   const handleSelectAll = () => {
-    if(selected.length) {
-      setSelected([])
+    if (selected.length) {
+      setSelected([]);
+    } else {
+      setSelected(issues.map((issue) => issue.refnumber));
     }
-    else {
-      setSelected(issues.map((issue) => issue.refnumber))
-    }
-  }
+  };
 
   useEffect(() => {
-    const abortController = new AbortController()
-    const signal = abortController.signal
+    const abortController = new AbortController();
+    const signal = abortController.signal;
 
     listAdminIssue({ year, vol, issue }, signal).then((data) => {
       if (data && data.error) {
-        console.error(data.error)
+        console.error(data.error);
       } else {
         data.sort((a, b) => parseInt(a.pagenumber) - parseInt(b.pagenumber));
-        setIssues(data)
+        setIssues(data);
       }
-    })
+    });
 
     return function cleanup() {
-      abortController.abort()
-    }
-  }, [])
+      abortController.abort();
+    };
+  }, []);
 
   return (
     <div>
       <div className="flex gap-6">
-      <Link className="p-2 text-center rounded w-24 bg-green-700 text-slate-200" to={`/admin/archives/add/${year}/${vol}/${issue}`}>Add New</Link>
-      { selected.length !== 0 && 
-        <>
-          <button className="p-2 text-center rounded w-24 bg-green-700  text-slate-200" onClick={() => saveIndexPage(selected)}>Index Page</button>
-          <button className="p-2 text-center rounded w-24 bg-green-700  text-slate-200" onClick={() => saveBulkCertificates(selected)}>Certificate</button>
-        </>
-      }
+        <Link
+          className="w-24 p-2 text-center bg-green-700 rounded text-slate-200"
+          to={`/admin/archives/add/${year}/${vol}/${issue}`}
+        >
+          Add New
+        </Link>
+        {selected.length !== 0 && (
+          <>
+            <button
+              className="w-24 p-2 text-center bg-green-700 rounded text-slate-200"
+              onClick={() => saveIndexPage(selected)}
+            >
+              Index Page
+            </button>
+            <button
+              className="w-24 p-2 text-center bg-green-700 rounded text-slate-200"
+              onClick={() => saveBulkCertificates(selected)}
+            >
+              Certificate
+            </button>
+          </>
+        )}
       </div>
-      <table className="mt-4 w-full border-collapse border">
+      <table className="w-full mt-4 border border-collapse">
         <thead>
           <tr>
-            <th className="bg-gray-200 text-sm border border-slate-400 p-2"><input type="checkbox" checked={selected.length !== 0 && selected.length === issues.length} onChange={handleSelectAll}/></th>
-            <th className="bg-gray-200 text-sm border border-slate-400 p-2 w-16">S. NO.</th>
-            <th className="bg-gray-200 text-sm border border-slate-400 p-2 w-32">REF. NUM.</th>
-            <th className="bg-gray-200 text-sm border border-slate-400 p-2">TITLE</th>
-            <th className="bg-gray-200 text-sm border border-slate-400 p-2">TRANSACTION ID</th>
-            <th className="bg-gray-200 text-sm border border-slate-400 p-2">PAGES</th>
-            <th className="bg-gray-200 text-sm border border-slate-400 p-2 w-48">Created At</th>
-            <th className="bg-gray-200 text-sm border border-slate-400 p-2 w-48">Modified At</th>
-            <th className="bg-gray-200 text-sm border border-slate-400 p-2">ACTION</th>
+            <th className="p-2 text-sm bg-gray-200 border border-slate-400">
+              <input
+                type="checkbox"
+                checked={
+                  selected.length !== 0 && selected.length === issues.length
+                }
+                onChange={handleSelectAll}
+              />
+            </th>
+            <th className="w-16 p-2 text-sm bg-gray-200 border border-slate-400">
+              S. NO.
+            </th>
+            <th className="w-32 p-2 text-sm bg-gray-200 border border-slate-400">
+              REF. NUM.
+            </th>
+            <th className="p-2 text-sm bg-gray-200 border border-slate-400">
+              TITLE
+            </th>
+            <th className="p-2 text-sm bg-gray-200 border border-slate-400">
+              TRANSACTION ID
+            </th>
+            <th className="p-2 text-sm bg-gray-200 border border-slate-400">
+              PAGES
+            </th>
+            <th className="w-48 p-2 text-sm bg-gray-200 border border-slate-400">
+              Created At
+            </th>
+            <th className="w-48 p-2 text-sm bg-gray-200 border border-slate-400">
+              Modified At
+            </th>
+            <th className="p-2 text-sm bg-gray-200 border border-slate-400">
+              ACTION
+            </th>
           </tr>
         </thead>
         <tbody>
           {issues.map((article, index) => {
             return (
               <tr key={`article-${index + 1}`}>
-                <td className="bg-white border border-slate-400 p-2 text-center"><input type="checkbox" checked={selected.includes(article.refnumber)} onChange={()=>{handleSelect(article.refnumber)}}/></td>
-                <td className="bg-white border border-slate-400 p-2">{index + 1}</td>
-                <td className="bg-white border border-slate-400 p-2">{article.refnumber}</td>
-                <td className="bg-white border border-slate-400 p-2"><div dangerouslySetInnerHTML={{ __html: decode(article.title) }}></div></td>
-                <td className="bg-white border border-slate-400 p-2">{article.txnid}</td>
-                <td className="bg-white border border-slate-400 p-2">{article.pagenumber}</td>
-                <td className="bg-white border border-slate-400 p-2 w-48">{parseDate(article.creation)}</td>
-                <td className="bg-white border border-slate-400 p-2 w-48">{parseDate(article.modification)}</td>
-                <td className="bg-white border border-slate-400 p-2"><Link className="text-green-700 font-bold" to={`/admin/archives/${article.id}`}>EDIT</Link></td>
+                <td className="p-2 text-center bg-white border border-slate-400">
+                  <input
+                    type="checkbox"
+                    checked={selected.includes(article.refnumber)}
+                    onChange={() => {
+                      handleSelect(article.refnumber);
+                    }}
+                  />
+                </td>
+                <td className="p-2 bg-white border border-slate-400">
+                  {index + 1}
+                </td>
+                <td className="p-2 bg-white border border-slate-400">
+                  {article.refnumber}
+                </td>
+                <td className="p-2 bg-white border border-slate-400">
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: cleanHtml(decode(article.title)),
+                    }}
+                  ></div>
+                </td>
+                <td className="p-2 bg-white border border-slate-400">
+                  {article.txnid}
+                </td>
+                <td className="p-2 bg-white border border-slate-400">
+                  {article.pagenumber}
+                </td>
+                <td className="w-48 p-2 bg-white border border-slate-400">
+                  {parseDate(article.creation)}
+                </td>
+                <td className="w-48 p-2 bg-white border border-slate-400">
+                  {parseDate(article.modification)}
+                </td>
+                <td className="p-2 bg-white border border-slate-400">
+                  <Link
+                    className="font-bold text-green-700"
+                    to={`/admin/archives/${article.id}`}
+                  >
+                    EDIT
+                  </Link>
+                </td>
               </tr>
-            )
-          })
-          }
+            );
+          })}
         </tbody>
       </table>
     </div>
-  )
+  );
 }
--- client/core/Issue.js
+++ client/core/Issue.js
@@ -1,16 +1,25 @@
 import React, { useState, useEffect, useContext } from "react";
 import PageTitle from "./PageTitle";
 import { useParams, Link } from "react-router-dom";
-import {listPublicIssue} from "../helper/api-archives";
+import { listPublicIssue } from "../helper/api-archives";
 import { GlobalContext } from "../context/GlobalContext";
 import { decode } from "html-entities";
+import sanitizeHtml from "sanitize-html";
+
+const cleanHtml = (data) => {
+  let cleanData = sanitizeHtml(data, {
+    allowedTags: ["i", "em"],
+    allowedAttributes: {},
+  });
+  return cleanData.replace(/\s+/g, " ");
+};
 
 export default function Archives(props) {
   const params = useParams();
-  const year = parseInt(params.year.match(/\d+/)[0], 10)
-  const vol = parseInt(params.vol.match(/\d+/)[0], 10)
-  const issue = parseInt(params.issue.match(/\d+/)[0], 10)
-  const { settings } = useContext(GlobalContext)
+  const year = parseInt(params.year.match(/\d+/)[0], 10);
+  const vol = parseInt(params.vol.match(/\d+/)[0], 10);
+  const issue = parseInt(params.issue.match(/\d+/)[0], 10);
+  const { settings } = useContext(GlobalContext);
 
   const [articles, setArticles] = useState([]);
 
@@ -18,19 +27,25 @@ export default function Archives(props) {
     const abortController = new AbortController();
     const signal = abortController.signal;
 
-    listPublicIssue({year: year, vol: vol, issue: issue}, signal).then((data) => {
-      if (data && !data.error) {
-        const enabledArticles = data.filter(article => article.refnumber && article.status === "enabled");
-        enabledArticles.sort((a, b) => parseInt(a.pagenumber) - parseInt(b.pagenumber));
-        setArticles(enabledArticles);
+    listPublicIssue({ year: year, vol: vol, issue: issue }, signal).then(
+      (data) => {
+        if (data && !data.error) {
+          const enabledArticles = data.filter(
+            (article) => article.refnumber && article.status === "enabled"
+          );
+          enabledArticles.sort(
+            (a, b) => parseInt(a.pagenumber) - parseInt(b.pagenumber)
+          );
+          setArticles(enabledArticles);
+        }
       }
-    });
+    );
 
     return function cleanup() {
       abortController.abort();
     };
   }, []);
- 
+
   return (
     <div className="page ">
       <PageTitle title={`VOL. ${vol}, ISSUE ${issue} (${year})`} />
@@ -44,31 +59,64 @@ export default function Archives(props) {
           </tr>
         </thead>
         <tbody>
-          {articles.length !== 0 && articles.map((article, index) => {
-            return <tr key={`article-${index + 1}`}>
-              <td>{index + 1}</td>
-              <td>
-                <div className="article-info">
-                  <div><b dangerouslySetInnerHTML={{ __html: decode(article.title) }}></b></div>
-                  <div><b>Authors: </b>{article.authorname}</div>
-                  <div className="abstract">
-                    <Link to={`/archives/${params.year}/${params.vol}/${params.issue}/${article.refnumber}`}>
-                      <b>Abstract</b>
-                    </Link>
-                    &nbsp;|&nbsp;
-                    {article.file && <><a href={`/assets/archives/${article.year}/vol${article.volume}issue${article.issue}/${article.file}`} download={`article-${article.refnumber}.pdf`}><b>Download</b></a>&nbsp;|&nbsp;</>}
-                   
-                    <div>Pages: {article.pagenumber}</div>
-                  </div>
-                  <div className="citation"><b>How to cite this article:</b></div>
-                  <div>{article.authroname} <b dangerouslySetInnerHTML={{ __html: decode(article.title) }}></b>. {settings.websitename}, Volume {vol}, Issue {issue}, {year}, Pages {article.pagenumber}</div>
-                </div>
-              </td>
-              <td>{article.subject}</td>
-              <td>{article.country}</td>
-            </tr>
-          })
-          }
+          {articles.length !== 0 &&
+            articles.map((article, index) => {
+              return (
+                <tr key={`article-${index + 1}`}>
+                  <td>{index + 1}</td>
+                  <td>
+                    <div className="article-info">
+                      <div>
+                        <b
+                          dangerouslySetInnerHTML={{
+                            __html: cleanHtml(decode(article.title)),
+                          }}
+                        ></b>
+                      </div>
+                      <div>
+                        <b>Authors: </b>
+                        {article.authorname}
+                      </div>
+                      <div className="abstract">
+                        <Link
+                          to={`/archives/${params.year}/${params.vol}/${params.issue}/${article.refnumber}`}
+                        >
+                          <b>Abstract</b>
+                        </Link>
+                        &nbsp;|&nbsp;
+                        {article.file && (
+                          <>
+                            <a
+                              href={`/assets/archives/${article.year}/vol${article.volume}issue${article.issue}/${article.file}`}
+                              download={`article-${article.refnumber}.pdf`}
+                            >
+                              <b>Download</b>
+                            </a>
+                            &nbsp;|&nbsp;
+                          </>
+                        )}
+                        <div>Pages: {article.pagenumber}</div>
+                      </div>
+                      <div className="citation">
+                        <b>How to cite this article:</b>
+                      </div>
+                      <div>
+                        {article.authroname}{" "}
+                        <b
+                          dangerouslySetInnerHTML={{
+                            __html: decode(article.title),
+                          }}
+                        ></b>
+                        . {settings.websitename}, Volume {vol}, Issue {issue},{" "}
+                        {year}, Pages {article.pagenumber}
+                      </div>
+                    </div>
+                  </td>
+                  <td>{article.subject}</td>
+                  <td>{article.country}</td>
+                </tr>
+              );
+            })}
         </tbody>
       </table>
     </div>
