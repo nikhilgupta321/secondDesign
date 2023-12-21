0a94593671a8d0691ea0781f3cc523215103cfea
nikhilgupta321 <nikhilgupta321@gmail.com>
2023-12-21 12:30:12 +0000

    editor photo

--- client/core/EditorSlot.js
+++ client/core/EditorSlot.js
@@ -5,9 +5,9 @@ export default function EditorSlot(props) {
   const editorPicture = props.editor.picture || "avatar.png";
   return (
     <div className="flex border-b-2 border-b-gray-600">
-      {/* <div className="w-1/6 p-2 border-r-2 border-blue-600">
+      <div className="p-2 border-r-2 border-blue-600 w-28 h-36 ">
         <img src={`assets/editors/${editorPicture}`} />
-      </div> */}
+      </div>
       <div className="w-5/6 p-2">
         <div>
           <b>{props.editor.name}</b>&nbsp;
