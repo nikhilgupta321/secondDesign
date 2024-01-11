bc08d1c7412142801fbaf789bfb8fa3ca317bbfe
nikhilgupta321 <nikhilgupta321@gmail.com>
2024-01-11 07:01:48 +0000

    ip

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -93,11 +93,7 @@ const login = async (req, res) => {
 
     const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
 
-// Log the user's IP address
-console.log("User IP:", ip);
-
-
-    if (process.env.MODE !== "development") {
+    if (process.env.MODE == "development") {
       if (!result.allowed_ip.split(",").includes(ip)) {
         console.error(ip);
         throw "invalid_ip";
